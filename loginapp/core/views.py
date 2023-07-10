3a9153187d5d8c0b9f0bad2d5c8c7bac1e2c43b9
Devunahsirk <100826392+Devunahsirk@users.noreply.github.com>
2023-07-10 23:13:05 +0530

    views and urls

--- loginapp/core/views.py
+++ loginapp/core/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.contrib import messsages
+
+
 
 # Create your views here.
+def home(request):    
+    return render(request, "home.html")
+def signup(request):
+
+    if request.method =="POST":
+        username=request.POST['username']
+        fname=request.POST['firstname']
+        lname=request.POST['lastname']
+        email=request.POST['email']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        myuser = User.objetcs.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messsages.success(request,"Your account is succesfully created")
+        return redirect('signin')
+
+
+
+
+
+    return render(request, "signup.html")
+def signin(request):
+    return render(request, "signin.html")
+def signout(request):
+    pass
+
