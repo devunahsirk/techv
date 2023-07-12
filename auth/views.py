0f3302460a6d2cc94776355eed7ec5cb520872ab
Devunahsirk <100826392+Devunahsirk@users.noreply.github.com>
2023-07-12 14:30:20 +0530

    mod1

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib import messsages
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 
 
@@ -18,13 +18,13 @@ def signup(request):
         pass1=request.POST['pass1']
         pass2=request.POST['pass2']
 
-        myuser = User.objetcs.create_user(username, email, pass1)
+        myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
 
         myuser.save()
 
-        messsages.success(request,"Your account is succesfully created")
+        messages.success(request,"Your account is succesfully created")
         return redirect('signin')
 
 
@@ -33,6 +33,21 @@ def signup(request):
 
     return render(request, "signup.html")
 def signin(request):
+    
+    if request.method =="POST":
+        username=request.POST['username']
+        pass1=request.POST['pass1']
+
+        user = authenticate(username=username,password=pass1)
+
+        if user is not None:
+            login(request,user)
+            fname= user.first_name
+            return render(request,"index.html",{'firstname':fname})
+        else:
+            messages.error(request,"Bad credentials")
+            return redirect ('home')
+
     return render(request, "signin.html")
 def signout(request):
     pass
