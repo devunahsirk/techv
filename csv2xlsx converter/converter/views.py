6a0bd586f9b0a1cd72da6aa042e1a570af087de9
Devunahsirk <100826392+Devunahsirk@users.noreply.github.com>
2023-07-16 16:51:08 +0530

    Optimised the converter

--- csv2xlsx converter/converter/views.py
+++ csv2xlsx converter/converter/views.py
@@ -5,31 +5,28 @@ from openpyxl import Workbook
 from io import BytesIO
 
 def home(request):
-    return render(request, 'converter/home.html')
+    return render(request, 'home.html')
+
 
 def convert(request):
-    if request.method == 'POST':
-        csv_file = request.FILES['csv_file']
+    if request.method == "POST":
+        csv_file = request.FILES['csv_file']  # Assuming the CSV file is uploaded using a form field named 'csv_file'
+
+        # Read the CSV file using pandas
         df = pd.read_csv(csv_file)
 
-        # Create a new workbook
-        workbook = Workbook()
-        sheet = workbook.active
+        # Convert the dataframe to an XLSX file
+        xlsx_file = pd.ExcelWriter('converted_file.xlsx', engine='xlsxwriter')
+        df.to_excel(xlsx_file, index=False)
+        xlsx_file.close()
 
-        # Write the CSV data to the workbook
-        for row_data in df.itertuples(index=False):
-            sheet.append(row_data)
-
-        # Save the workbook in memory
-        output = BytesIO()
-        workbook.save(output)
-        output.seek(0)
-
-        # Create the HTTP response with the XLSX file
+        # Create the response with the XLSX file
         response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = 'attachment; filename=converted.xlsx'
-        response.write(output.getvalue())
+        response['Content-Disposition'] = 'attachment; filename="converted_file.xlsx"'
+
+        with open('converted_file.xlsx', 'rb') as file:
+            response.write(file.read())
 
         return response
 
-    return render(request, 'converter/home.html')
+    return render(request, "auth/signup.html")
