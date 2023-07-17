8ec190c5e1c85e8471a6b3a9310f3a29c5bfe0c2
Devunahsirk <100826392+Devunahsirk@users.noreply.github.com>
2023-07-17 23:10:43 +0530

    mod

--- loginapp/auth/views.py
+++ loginapp/auth/views.py
@@ -1,54 +1,36 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate,login
-
-
 
 # Create your views here.
-def home(request):    
-    return render(request, "home.html")
+def home(request):
+    return render(request,"auth/index.html")
+
 def signup(request):
 
-    if request.method =="POST":
-        username=request.POST['username']
-        fname=request.POST['firstname']
-        lname=request.POST['lastname']
-        email=request.POST['email']
-        pass1=request.POST['pass1']
-        pass2=request.POST['pass2']
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
 
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name=fname
+        myuser.last_name=lname
 
-        myuser.save()
+        messages.success(request, "Your account is created")
 
-        messages.success(request,"Your account is succesfully created")
         return redirect('signin')
 
 
 
 
+    return render(request,"auth/signup.html")
 
-    return render(request, "signup.html")
 def signin(request):
-    
-    if request.method =="POST":
-        username=request.POST['username']
-        pass1=request.POST['pass1']
+    return render(request,"auth/signin.html")
 
-        user = authenticate(username=username,password=pass1)
-
-        if user is not None:
-            login(request,user)
-            fname= user.first_name
-            return render(request,"index.html",{'firstname':fname})
-        else:
-            messages.error(request,"Bad credentials")
-            return redirect ('home')
-
-    return render(request, "signin.html")
 def signout(request):
-    pass
-
+    pass    
