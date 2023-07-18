1b6b46e315b1d514757d1cdd478351a3d96838a2
Devunahsirk <100826392+Devunahsirk@users.noreply.github.com>
2023-07-18 22:41:03 +0530

    converter  mod

--- csv2xlsx converter/converter/views.py
+++ csv2xlsx converter/converter/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
-from openpyxl import Workbook
-from io import BytesIO
+
 
 def home(request):
     return render(request, 'home.html')
@@ -10,17 +9,16 @@ def home(request):
 
 def convert(request):
     if request.method == "POST":
-        csv_file = request.FILES['csv_file']  # Assuming the CSV file is uploaded using a form field named 'csv_file'
+        csv_file = request.FILES['csv_file'] 
 
-        # Read the CSV file using pandas
         df = pd.read_csv(csv_file)
 
-        # Convert the dataframe to an XLSX file
+        # Convert 
         xlsx_file = pd.ExcelWriter('converted_file.xlsx', engine='xlsxwriter')
         df.to_excel(xlsx_file, index=False)
         xlsx_file.close()
 
-        # Create the response with the XLSX file
+        # Response 
         response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         response['Content-Disposition'] = 'attachment; filename="converted_file.xlsx"'
 
@@ -29,4 +27,4 @@ def convert(request):
 
         return response
 
-    return render(request, "auth/signup.html")
+   
